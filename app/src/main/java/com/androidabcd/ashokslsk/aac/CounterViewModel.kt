f7780d8dea7f06fa4c6ac3dabcc207854f6a10b7
Ashok Kumar S <ashokslsk@gmail.com>
2019-03-28 14:57:25 +0530

    Data binding

---
+++ app/src/main/java/com/androidabcd/ashokslsk/aac/CounterViewModel.kt
@@ -0,0 +1,17 @@
+package com.androidabcd.ashokslsk.aac
+
+import android.databinding.ObservableField
+import java.util.*
+
+/**
+ * Created by Srinivasa Ashok Kumar on 28/03/19.
+ */
+
+class CounterViewModel{
+    var counter = 0
+    var clicked = ObservableField("Clicked "+counter+" Times")
+    fun CounterClicked(){
+        counter++
+        clicked.set("Clicked "+counter+" Times")
+    }
+}
